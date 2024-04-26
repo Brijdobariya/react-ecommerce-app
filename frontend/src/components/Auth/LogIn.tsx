a5ceb8861693ee3cd9ef5679cafe5f3b70aac7bb
Brij Dobariya <127536396+Brijdobariya@users.noreply.github.com>
2024-04-26 17:11:22 +0530

    00

--- frontend/src/components/Auth/LogIn.tsx
+++ frontend/src/components/Auth/LogIn.tsx
@@ -1,7 +1,75 @@
-import React from "react";
+import type { FormProps } from 'antd';
+import { Button, Checkbox, Form, Input } from 'antd';
+import React from 'react';
 
-const LogIn = () => {
-  return <div>LogIn</div>;
+type FieldType = {
+  username?: string;
+  password?: string;
+  remember?: string;
 };
 
-export default LogIn;
+const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+  console.log('Success:', values);
+};
+
+const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
+
+
+const LogIn: React.FC = () => {
+  return (
+  <>
+   
+
+  <div className="w-screen h-screen flex justify-center items-center">
+  
+  <Form
+    name="basic"
+    labelCol={{ span: 8 }}
+    wrapperCol={{ span: 16 }}
+   
+    initialValues={{ remember: true }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+    className='w-[40%]'
+    >
+
+
+    <Form.Item<FieldType>
+      label="Username"
+      name="username"
+      rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+      <Input />
+    </Form.Item>
+
+    <Form.Item<FieldType>
+      label="Password"
+      name="password"
+      rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+      <Input.Password />
+    </Form.Item>
+
+    <Form.Item<FieldType>
+      name="remember"
+      valuePropName="checked"
+      wrapperCol={{ offset: 8, span: 16 }}
+    >
+      <Checkbox>Remember me</Checkbox>
+    </Form.Item>
+
+    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>  
+      </div>
+  </>
+  )
+}
+
+export default LogIn
