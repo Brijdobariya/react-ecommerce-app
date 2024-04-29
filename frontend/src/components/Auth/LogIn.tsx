99663d0e372c893fc44244748f0908b72d49eb01
HarshulKachariya <harshulkachariya17@gmail.com>
2024-04-29 11:12:42 +0530

    ghfhfh

---
+++ frontend/src/components/Auth/LogIn.tsx
@@ -0,0 +1,66 @@
+import type { FormProps } from "antd";
+import { Button, Form, Input } from "antd";
+import React from "react";
+import { useAuth } from "../../context/AuthContext";
+
+type FieldType = {
+  username?: string;
+  password?: string;
+  remember?: string;
+};
+
+const LogIn: React.FC = () => {
+  const { Login } = useAuth();
+
+  const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+    console.log("Success:", values);
+    const { username, password } = values;
+    Login(username, password);
+  };
+
+  const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (
+    errorInfo
+  ) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <>
+      <div className="w-screen h-screen flex justify-center items-center">
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+          className="w-[40%]"
+        >
+          <Form.Item<FieldType>
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item<FieldType>
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              SignIn
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default LogIn;
--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -5,33 +5,13 @@ const AuthContext = createContext();
 export const useAuth = () => useContext(AuthContext);
 
 const AuthContextProvider: React.FC = ({ children }: any) => {
-<<<<<<< Updated upstream
-
-const login=async(formData:any)=>{
-
-  console.log("your data",formData)
-
-const username =formData.username;
-const password =formData.password;
-console.log(username)
-
-
-
-
-}
-
-
-
-  const value = {login};
-=======
-  const Login: any = (username: any, password: any) => {
-    // const username = values.username;
-    // const password = values.password;
-    console.log("username", username, "password", password);
+  const Login = (username, password) => {
+    console.log("usrname", username, "password", password);
   };
 
-  const value = { Login };
->>>>>>> Stashed changes
+  const value = {
+    Login,
+  };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
